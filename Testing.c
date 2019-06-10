98cdc0413baa52b5d3efcece399a0c65c40a0770
dmessier7 <daniel.messier.11.12@gmail.com>
2019-06-10 15:41:13 -0700

    AVR Library Attempt

---
+++ Testing.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+#include</Users/danielmessier/Documents/Daniel-Messier-C-Library/avr/io.h>
+
+int main()
+{
+DDRC = 0xFF;
+PORTC = 0x00;
+while(1)
+{
+
+}
+}
